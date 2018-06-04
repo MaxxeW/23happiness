f4296e36c06d2fbda25d7ef8c8e399eb2ccc3fc4
Max Huang <maxfr924@gmail.com>
2018-06-04 22:49:10 +0800

    Add p5js library

---
+++ js/sketch.js
@@ -0,0 +1,26 @@
+function loadFile() {
+  loadStrings("23happiness.txt", fileLoaded);
+};
+
+function fileLoaded(data) {
+  createP(join(data, "<br/>"));
+};
+
+function fileSelected(file) {
+  createP(file.name + " " + file.size + " " + file.type);
+
+  if (file.type == "text") {
+    createP(file.data);
+  } else {
+    createP("I need a text file.");
+  };
+};
+
+function setup() {
+  noCanvas();
+
+  createFileInput(fileSelected);
+
+  var button = select("#loadfile");
+  button.mousePressed(loadFile);
+};
