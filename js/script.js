55c3056b85017ec7f25369fee649b63390b38f2f
Max Huang <maxfr924@gmail.com>
2018-06-05 00:48:58 +0800

    Arrange button function

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
-
-
+function setup() {
+  // initial p5js
+  noCanvas();
+};
 // Set the date we're counting down to
-var countDownDate = new Date("Jun 4, 2018 23:48:00").getTime();
+var countDownDate = new Date("Jun 5, 2018 00:48:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -19,36 +21,32 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="demo"
-    document.getElementById("clock").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
+    document.getElementById("clock").innerHTML = "還有 " + days + "天 " + hours + "時 "
+    + minutes + "分 " + seconds + "秒 ";
 
     // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
 
         document.getElementById("display").innerHTML = "here";
-        createButton();
+        createHeart();
 
 
-
-        function loadFile() {
-          loadStrings("23happiness.txt", fileLoaded);
-        };
-
-        function fileLoaded(data) {
-          createP(join(data, "<br/>"));
-        };
-
-        function setup() {
-          noCanvas();
-          var button = select("#loadfile");
-          button.mousePressed(loadFile);
-        };
     }
 }, 1000);
 
-function createButton() {
-  var $button = $('<button id="loadfile" value="new button" />');
-  $button.appendTo($("#display"));
+function loadFile() {
+  loadStrings("23happiness.txt", fileLoaded);
+};
 
-}
+function fileLoaded(data) {
+  createP(join(data, "<br/>"));
+};
+
+function createHeart() {
+  var $button = $('<button id="demo" value="new button" />');
+  $button.appendTo($("#display"));
+  var click = select("#demo");
+  click.mousePressed(loadFile);
+
+};
